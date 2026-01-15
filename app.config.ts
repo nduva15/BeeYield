05b33d2e01b24023fb6206b971259582123a6179
cebaszn <timothy.mathuva@strathmore.edu>
2026-01-16 02:15:34 +0300

    fix: add vite-tsconfig-paths to resolve path aliases

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from '@tanstack/start/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     tsr: {
         appDirectory: 'src',
+    },
+    vite: {
+        plugins: [
+            tsconfigPaths(),
+        ],
     },
 })
