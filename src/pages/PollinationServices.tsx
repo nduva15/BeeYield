bf3a4f387e284d27a25f680ef451416eee5ae157
mikengei <timothy.mathuva@strathmore.edu>
2026-09-20 11:36:49 +0300

    chore(home): purge unused section comment and finalize home page shortening

--- src/pages/PollinationServices.tsx
+++ src/pages/PollinationServices.tsx
@@ -305,9 +305,6 @@ const PollinationServices = () => {
                 </div>
             </section>
 
-
-            {/* Verified Paths Section */}
-
 {/* The BeeYield Difference */}
             <section className="py-32 bg-background relative overflow-hidden">
                 <div className="container mx-auto px-4">
